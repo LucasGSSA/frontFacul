c25ca1dbe009de2fd8b28322e4b31f600be9010a
LukanONE <lucasgabriel.silveira10@gmail.com>
2023-12-06 02:39:40 -0300

    a

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
     });
+  });
 });
-
 
 function toggleDarkMode() {
   document.body.classList.toggle("dark-mode");
