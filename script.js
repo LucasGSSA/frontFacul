202412388859c34496b1a1c30a541bc83b2089b9
LukanONE <lucasgabriel.silveira10@gmail.com>
2023-12-06 02:33:00 -0300

    meis ou menos

--- script.js
+++ script.js
@@ -1,32 +1,14 @@
-// Função para carregar conteúdo na página principal
-function loadContent(page) {
-    const mainContent = document.querySelector('main');
-    // Lógica para carregar conteúdo com base na página
-    // Exemplo: mainContent.innerHTML = '<p>Conteúdo da página principal</p>';
-}
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
 
-// Exemplo de manipulação do DOM para aumentar o tamanho da fonte
-function increaseFontSize() {
-    const mainContent = document.querySelector('main');
-    const currentSize = window.getComputedStyle(mainContent).fontSize;
-    const newSize = (parseFloat(currentSize) * 1.2) + 'px';
-    mainContent.style.fontSize = newSize;
-}
-
-// Exemplo de animação ao passar o mouse sobre um elemento
-function animateOnHover(element) {
-    element.addEventListener('mouseover', () => {
-        element.style.transform = 'scale(1.1)';
-        element.style.transition = 'transform 0.3s ease';
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
-
-    element.addEventListener('mouseout', () => {
-        element.style.transform = 'scale(1)';
-    });
-}
-
-// Chame as funções de manipulação do DOM conforme necessário
-document.addEventListener('DOMContentLoaded', () => {
-    // Exemplo: loadContent('home');
-    // Exemplo: animateOnHover(document.querySelector('.hover-element'));
 });
+
+// Adicione isso ao seu arquivo JavaScript (script.js)
+function toggleDarkMode() {
+  document.body.classList.toggle("dark-mode");
+}
